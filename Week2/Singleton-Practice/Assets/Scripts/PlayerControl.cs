818fa66111072cd093834fc8064a6f4d9a31da33
Eric Jiahuan Cheng <jc11769@nyu.edu>
2023-02-05 20:43:52 -0500

    Single Player practice

--- Week2/Singleton-Practice/Assets/Scripts/PlayerControl.cs
+++ Week2/Singleton-Practice/Assets/Scripts/PlayerControl.cs
@@ -1,23 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    // Singleton player
+    public static PlayerControl _PlayerControl;
+    
     private Rigidbody2D _rd2D;
 
     // set applied force amount
     [SerializeField] private int forceAmount = 2;
+
+    // Singleton control
+    private void Awake()
+    {
+        if (_PlayerControl == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            _PlayerControl = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        // get RigidBody2D component from gameobject
+        // get RigidBody2D component from gameObject
         _rd2D = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // WASD control
         if (Input.GetKey(KeyCode.W))
         {
             _rd2D.AddForce(Vector2.up * forceAmount);
@@ -37,6 +57,10 @@ public class PlayerControl : MonoBehaviour
         {
             _rd2D.AddForce(Vector2.right * forceAmount);
         }
+        
+        // slow done object speed by time
+        _rd2D.velocity *= 0.99f;
+
     }
         
 }
